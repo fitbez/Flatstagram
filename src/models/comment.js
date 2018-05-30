d65697952fb2db22de4f5d62841091bd51741c0c
fitsum belayneh <fitsu_alt@yahoo.com>
2018-05-30 14:10:21 -0400

    comment class created

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,1 +1,9 @@
 // create Comment class here
+class Comment {
+  constructor(commentContent, imageId) {
+    this.id = Comment.all.length;
+    this.imageId = imageId;
+    this.commentContent = commentContent;
+    Comment.all.push(this)
+  }
+}
